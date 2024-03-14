17340cca28b70b570be95fd04014d99e39e5e2a2
James Allsopp <j.allsopp@bham.ac.uk>
2024-03-14 04:31:56 +0000

    Added writing out to a pickle for testing

--- import_data.py
+++ import_data.py
@@ -1,5 +1,6 @@
 import generate_person
 import database_models
+import read_write_pickle
 
 from sqlalchemy.orm import Session
 
@@ -7,12 +8,14 @@ number_of_people_to_generate = 20
 
 people = generate_person.get_schema_definition(20)
 
+people_list = []
 with Session(database_models.engine) as session:
-    people_list = []
     for p in people:
-        people_list.append(database_models.User(uid=p["uid"], name=p["name"], favourite_food=p["favourite_food"], version=p["version"]))
+        people_list.append(database_models.User(uid=p["uid"],
+                                                name=p["name"],
+                                                favourite_food=p["favourite_food"],
+                                                version=p["version"]))
 
     session.add_all(people_list)
     session.commit()
-    
-        
+    read_write_pickle.save_pickle(people_list)
